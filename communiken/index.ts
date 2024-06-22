1ae515f0f8b034f03f8d457d9a274431cbce8655
Ignacio Morales <eduardo.moraless@usm.cl>
2024-06-22 15:39:52 -0400

    Update index.ts

--- communiken/index.ts
+++ communiken/index.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { Elysia, t } from 'elysia'
 
+const app = new Elysia()
+    .get('/', () => 'Hello Elysia')
+    .listen(8000)
 
+console.log(`🦊 Elysia is running at on port ${app.server?.port}...`)
